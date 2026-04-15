7803f2bf550b6ecab3b6427ef8501d2fd3ee1e17
Kyle Wiens <kyle@ifixit.com>
2026-04-14 20:33:54 -0400

    Hide River Walk SORBA trail that conflicts with Riverwalk routes

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -232,7 +232,7 @@ const RATING_COLOR_EXPRESSION: mapboxgl.Expression = [
 
 // Trails in the SORBA tileset that overlap with bike route layers and should
 // be hidden so they don't intercept clicks or render on top of routes.
-const HIDDEN_TRAILS = ['Tennessee Riverwalk'];
+const HIDDEN_TRAILS = ['Tennessee Riverwalk', 'River Walk'];
 
 const TRAIL_LAYERS: TrailLayerConfig[] = [
   {
